093b49ad506849c42dd6ded45f08afad91ffdff1
MeesterMarcus <lorenzanamarcus@gmail.com>
2021-05-30 11:01:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const testData = [
+    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
+
+
+const CardList = (props) => (
+    <div>
+        {testData.map(profile => <Card {...profile}/>)}
     </div>
-  );
+);
+
+class Card extends React.Component {
+    render() {
+        const profile = this.props;
+        return (
+            <div className="github-profile">
+                <img alt="profile" src={profile.avatar_url}/>
+                <div className="info">
+                    <div className="name">{profile.name}</div>
+                    <div className="company">{profile.company}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <CardList />
+            </div>
+        );
+    }
 }
 
 export default App;
